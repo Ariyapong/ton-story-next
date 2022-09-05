0d059a15d919b3080e6ca961d6d0d33e4aa9079a
Tony <ariyapongw.ton@gmail.com>
2022-09-06 00:35:38 +0700

    config styles

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = async (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig = {
+    /* config options here */
+
+  };
+  console.log("check next config : ", nextConfig)
+  return nextConfig;
+};
